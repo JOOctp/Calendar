6c45192fe6f0bcea6acab4c477bdbcbba843b139
Jevon Putra <>
2023-12-22 20:50:00 +0700

    example implementation

--- app/src/main/java/com/jop/calendarview/MainActivity.kt
+++ app/src/main/java/com/jop/calendarview/MainActivity.kt
@@ -2,10 +2,23 @@ package com.jop.calendarview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jop.calendar.CalendarView
+import com.jop.calendarview.databinding.ActivityMainBinding
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.apply {
+            calendar.setCalenderViewListener(object : CalendarView.CalendarViewListener{
+                override fun onSelectedDate(date: Date) {
+                    tvSelectDate.text = date.toString()
+                }
+            })
+        }
     }
 }
